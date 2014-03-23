8af3c9570b810a3276ae11bbfc992dc99bded3bb
Benjamin <benjamin.siklosi@gmail.com>
2014-03-23 19:35:31 +0100

    Main change commit

--- src/Monopoly/Main.java
+++ src/Monopoly/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //Main
     @Override
     public void start(Stage primaryStage) throws Exception{
 		Parent root = FXMLLoader.load(getClass().getResource("View/GameWindow.fxml"));
